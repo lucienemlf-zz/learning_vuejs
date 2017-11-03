1734cb33066258ae3fab7cd09348ed56ada77e4d
Maria Luciene <maria.felix@ignicaodigital.com.br>
2017-11-03 17:07:45 -0200

    Exercício

--- projects/1 challenge/app.js
+++ projects/1 challenge/app.js
@@ -12,15 +12,13 @@ new Vue({
     play(type) {
 
       if(type == 'attack') {
-        this.attack(1, 5);
+        this.attack(1, 10);
       } else if (type == 'special-attack') {
-        this.attack(6, 10);
+        this.attack(11, 25);
       } else if(type == 'heal') {
-        this.attack(11, 15);
+        this.heal();
       } else if(type == 'give-up') {
-        this.attacks = [];
-        this.monster_life = 100;
-        this.you_life = 100;
+        this.startGame();
       }
     },
 
@@ -28,15 +26,44 @@ new Vue({
       const monster_value = this.getRandomValue(min_value, max_value);
       const player_value = this.getRandomValue(min_value, max_value);
 
-      this.monster_life = this.monster_life - monster_value;
-      this.you_life = this.you_life - player_value;
+      this.monster_life -= monster_value;
+      this.you_life -= player_value;
 
       this.attacks.push(`${this.monster_attack} ${monster_value}`);
       this.attacks.push(`${this.player_attack} ${player_value}`);
+      this.checkWin()
     },
 
     getRandomValue(min_value, max_value) {
       return Math.floor((Math.random() * max_value) + min_value);
+    },
+
+    startGame() {
+      this.attacks = [];
+      this.monster_life = 100;
+      this.you_life = 100;
+      this.new_game = true;
+    },
+
+    checkWin() {
+      let message = '';
+      if(this.monster_life <= 0 || this.you_life <= 0) {
+        if(this.monster_life < this.you_life) {
+          message = 'Monster Win';
+        }else {
+          message = 'You Win';
+        }
+        alert(message);
+        this.startGame();
+      }
+    },
+
+    heal() {
+      if(this.you_life <= 90) {
+        this.you_life += 10;
+      } else {
+        this.you_life = 100;
+      }
     }
   }
 })
