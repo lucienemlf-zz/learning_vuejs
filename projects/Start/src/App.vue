bd40628e59686e3fcb532aa687730dfba5620a1a
Maria Luciene <marialucienefelix@gmail.com>
2018-02-02 16:58:44 -0200

    modulo 7

---
+++ projects/Start/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <Header></Header>
+        <div class="row">
+            <ServerStatus></ServerStatus>
+            <ServerDetails></ServerDetails>
+        </div>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './components/server/ServerStatus.vue'
+    import ServerDetails from './components/server/ServerDetails.vue'
+    import Header from './components/shared/Header.vue'
+    import Footer from './components/shared/Footer.vue'
+
+    export default {
+        components: {
+            Header,
+            Footer,
+            ServerStatus,
+            ServerDetails,
+        }
+    }
+</script>
+
+<style>
+
+</style>
