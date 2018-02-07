8448563e85378831977c900ce94b2cce62297cfe
Maria Luciene <marialucienefelix@gmail.com>
2018-02-07 14:08:02 -0200

    section 9 - slots and dynamic components

---
+++ projects/DinamicComponent/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button
+                    @click="switchComponent('appBlue')"
+                    class="btn btn-primary">Load Blue Template</button>
+                <button
+                    @click="switchComponent('appGreen')"
+                    class="btn btn-success">Load Green Template</button>
+                <button
+                    @click="switchComponent('appRed')"
+                    class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <h2 slot="header">Header</h2>
+                        <h2>Vai ser renderizado no filho</h2>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data() {
+            return {
+                selectedComponent: 'appBlue',
+            };
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        methods: {
+            switchComponent(component) {
+                this.selectedComponent = component;
+                console.log('component name', this.selectedComponent)
+            },
+        }
+    }
+</script>
+
+<style>
+</style>
