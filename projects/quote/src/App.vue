9692a8c6dc98fa9b34c0bdf6c72de92a34228b29
Maria Luciene <marialucienefelix@gmail.com>
2018-02-14 18:04:46 -0200

    section 10 - quote

---
+++ projects/quote/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Quote></Quote>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Quote,
+  },
+}
+</script>
+
+<style>
+</style>
